c08ec125f1739a6b194906df86ea0fb000c3adb3
Ranjith Mahendran <rmahendr@terpmail.umd.edu>
2025-12-04 16:59:18 -0500

    implemented pyramid matching round

--- game.py
+++ game.py
@@ -2,10 +2,12 @@
 
 from cards import create_deck
 from initial_guess_phase import initial_guess_phase
+from pyramid_matching import pyramid_round
 
 def main():
     deck = create_deck()
-    initial_guess_phase(deck)
+    hand = initial_guess_phase(deck)
+    matches = pyramid_round(deck,hand)
 
 if __name__ == "__main__":
     main()
