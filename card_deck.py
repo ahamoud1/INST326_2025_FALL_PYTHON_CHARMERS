397315c3b9a1f136dbaf42fbab200cca07be2959
Ranjith Mahendran <rmahendr@terpmail.umd.edu>
2025-12-04 15:48:16 -0500

    renamed file

---
+++ card_deck.py
@@ -0,0 +1,35 @@
+"""Creates a card object and a deck of random cards!"""
+
+import random
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        if self.rank == 11:
+            rank_name = "Jack"
+        elif self.rank == 12:
+            rank_name = "Queen"
+        elif self.rank == 13:
+            rank_name = "King"
+        elif self.rank == 14:
+            rank_name = "Ace"
+        else:
+            rank_name = str(self.rank)
+        return rank_name + " of " + self.suit
+
+    def __repr__(self):
+        return str(self)
+
+
+def create_deck():
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    ranks = list(range(2, 15))
+    deck = []
+    for suit in suits:
+        for rank in ranks:
+            deck.append(Card(rank, suit))
+    random.shuffle(deck)
+    return deck
