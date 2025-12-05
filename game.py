9e9bfaf921545bf27ad89337fbefc62dcf9f643b
Ranjith Mahendran <rmahendr@terpmail.umd.edu>
2025-12-05 15:37:31 -0500

    added multiplayer functionality to first two phases

--- game.py
+++ game.py
@@ -1,19 +1,34 @@
 """runs game"""
 
 from cards import create_deck
-from argparse import ArgumentParser
 from initial_guess_phase import initial_guess_phase
 from pyramid_matching import pyramid_round
+from players import Player
 
 def main():
     deck = create_deck()
-    hand = initial_guess_phase(deck)
-    matches = pyramid_round(deck,hand)
-    
-def parse_args(arglist):
-    parser = ArgumentParser()
-    parser.add_argument("names", nargs="+", help="names of players")
-    return parser.parse_args(arglist)
+
+    num_players = int(input("Enter number of players: "))
+    players = []
+
+    for i in range(num_players):
+        name = input("Enter name for player " + str(i + 1) + ": ")
+        player = Player(name)
+        players.append(player)
+
+    for player in players:
+        print("\nPlayer:", player.name)
+        hand = initial_guess_phase(deck)
+        player.set_hand(hand)
+
+        matches = pyramid_round(deck, player.hand)
+        player.set_matches(matches)
+
+    print("\nGame over. Summary:")
+    for player in players:
+        print(f"\nPlayer: {player.name}")
+        print(f"Hand: {player.hand}")
+        print(f"Matches: {player.matches}")
 
 if __name__ == "__main__":
     main()
