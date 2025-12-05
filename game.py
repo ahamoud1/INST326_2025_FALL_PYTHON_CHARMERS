834196fa4b6576db29a1dbb220a58be39453579e
Carlos Miranda <canthonym2004@gmail.com>
2025-12-05 13:32:58 -0500

    added parse_args

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 """runs game"""
 
 from cards import create_deck
+from argparse import ArgumentParser
 from initial_guess_phase import initial_guess_phase
 from pyramid_matching import pyramid_round
 
@@ -8,6 +9,11 @@ def main():
     deck = create_deck()
     hand = initial_guess_phase(deck)
     matches = pyramid_round(deck,hand)
+    
+def parse_args(arglist):
+    parser = ArgumentParser()
+    parser.add_argument("names", nargs="+", help="names of players")
+    return parser.parse_args(arglist)
 
 if __name__ == "__main__":
     main()
