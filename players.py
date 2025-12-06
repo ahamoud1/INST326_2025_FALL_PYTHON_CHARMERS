b5a3332dca39f4a8828321de2f1784024e152148
Ranjith Mahendran <rmahendr@terpmail.umd.edu>
2025-12-05 19:21:09 -0500

    removed unused function

--- players.py
+++ players.py
@@ -7,8 +7,5 @@ class Player:
     def set_hand(self, cards):
         self.hand = cards
 
-    def set_matches(self, cards):
-        self.matches = cards
-
     def __str__(self):
         return self.name
