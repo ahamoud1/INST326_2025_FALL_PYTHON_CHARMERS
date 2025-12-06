57e7bf0e4b5eec57a3cecdfe79e39979d6dc333d
Ranjith Mahendran <rmahendr@terpmail.umd.edu>
2025-12-05 20:49:58 -0500

    fixed typo

--- riding_the_bus.py
+++ riding_the_bus.py
@@ -1,4 +1,4 @@
-from initial_guess_phase import play_guess_round
+from initial_guess_phase import initial_guess_phase
 
 def ride_the_bus_phase(deck, players):
     bus_player = max(players, key=lambda p: len(p.matches))
@@ -8,7 +8,7 @@ def ride_the_bus_phase(deck, players):
 
     while not finished and len(deck) >= 4:
         print("\nNew attempt: ")
-        cards, all_correct = play_guess_round(deck)
+        cards, all_correct = initial_guess_phase(deck)
 
         if all_correct:
             finished = True
