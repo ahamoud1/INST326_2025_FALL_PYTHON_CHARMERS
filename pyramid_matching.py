65b0139fa18cb935b1f71a68ebbf0c8599a2b7e0
Ranjith Mahendran <rmahendr@terpmail.umd.edu>
2025-12-05 19:20:33 -0500

    made pyramid shared for players

--- pyramid_matching.py
+++ pyramid_matching.py
@@ -1,12 +1,11 @@
-def pyramid_round(deck, hand):
-    matches = []
+def pyramid_round(deck, players):
+    for p in players:
+        p.matches = []
 
-    for i in range(10):
+    for i in range(10):   
         card = deck.pop()
-        for h in hand:
-            if h.rank == card.rank:
-                matches.append(card)
-                break
-
-    print("Number of matches in pyramid round: ", len(matches))
-    return matches
+        for p in players:
+            for h in p.hand:
+                if h.rank == card.rank:
+                    p.matches.append(card)
+                    break  
