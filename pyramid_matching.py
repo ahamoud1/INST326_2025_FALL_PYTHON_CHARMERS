5ce36c4c178bcbf3007ee9ab00e40033e889f5f6
Ranjith Mahendran <rmahendr@terpmail.umd.edu>
2025-12-08 13:05:32 -0500

    updated scoring for pyramid round

--- pyramid_matching.py
+++ pyramid_matching.py
@@ -6,12 +6,13 @@ def pyramid_round(deck, players):
     row = 1
 
     for size in row_sizes:
+        points = len(row_sizes) - (row - 1)
         for i in range(size):   
             card = deck.pop()
             for p in players:
                 for h in p.hand:
                     if h.rank == card.rank:
                         p.matches.append((card, row))
-                        p.add_score(row)
+                        p.add_score(points)
                         break  
         row += 1
