1ad022b1e492b7d73391bca9f8a7f5f24589c333
Ranjith Mahendran <rmahendr@terpmail.umd.edu>
2025-12-08 17:21:31 -0500

    fixed riding the bus logic

--- riding_the_bus.py
+++ riding_the_bus.py
@@ -1,28 +1,32 @@
-import random
-
 def ride_the_bus(deck, players):
-    bus_player = min(players, key=players.score)
+    bus_player = min(players, key=lambda p: p.score)
     print(f"\n{bus_player.name} is riding the bus")
 
-    card_choices = []
-    
+    cards = []
     for i in range(10):
-        card_choices += random.choice(deck)
-        
-    for k in range(3):
-        card_num = input("Pick a card (1-10)").strip()
-        while (card_num+1) < 0 or (card_num+1) > 10:
-            print("Invalid input! Enter a number between 1 and 10")
-            card_num = input("Pick a card (1-10)").strip()
-        if card_choices.rank(card_num) == "Ace":
-            player_points += 4
-        elif card_choices.rank(card_num) == "King":
-            player_points += 3
-        elif card_choices.rank(card_num) == "Queen":
-            player_points += 2
-        elif card_choices.rank(card_num) == "Jack":
-            player_points +=1
-        else:
-            player_points += 0
-    print (f"\n{bus_player.name} finished Riding the Bus!")
-    return player_points
+        card = deck.pop()
+        cards.append(card)
+
+    values = {
+        "Ace": 4,
+        "King": 3,
+        "Queen": 2,
+        "Jack": 1
+    }
+
+    points = 0
+
+    for pick in range(3):
+        card_num = int(input("Pick a card (1-10): "))
+        while card_num not in range(1, 11):
+            card_num = int(input("Invalid! Pick a card (1-10): "))
+
+        card_index = card_num - 1
+        chosen_card = cards[card_index]
+
+        points += values.get(chosen_card.rank, 0)
+
+    bus_player.add_score(points)
+
+    print(f"{bus_player.name} earned {points} points!")
+
