75fdf93f0971d409ae6fce23af1ed44d5fd6aabc
Ayah Hamouda <ahamoud1@terpmail.umd.edu>
2025-12-07 20:32:44 -0500

    Ayah adds score attribute and score method

--- players.py
+++ players.py
@@ -3,9 +3,13 @@ class Player:
         self.name = name
         self.hand = []
         self.matches = []
+        self.score = 0
 
     def set_hand(self, cards):
         self.hand = cards
 
+    def add_score(self, points):
+        self.score += points
+        
     def __str__(self):
         return self.name
