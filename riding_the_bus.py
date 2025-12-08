349582cbe8525dfd5d8dfcddabc039d914ee75cb
Carlos Miranda <canthonym2004@gmail.com>
2025-12-08 16:40:35 -0500

    updated riding the bus round

--- riding_the_bus.py
+++ riding_the_bus.py
@@ -1,19 +1,28 @@
-from initial_guess_phase import guesses
+import random
 
 def ride_the_bus(deck, players):
-    bus_player = max(players, key=lambda p: len(p.matches))
+    bus_player = min(players, key=players.score)
     print(f"\n{bus_player.name} is riding the bus")
 
-    finished = False
-
-    while not finished and len(deck) >= 4:
-        print("\nNew attempt: ")
-        cards, all_correct, score = guesses(deck)
-        bus_player.add_score(score)
+    card_choices = []
+    
+    for i in range(10):
+        card_choices += random.choice(deck)
         
-        if all_correct:
-            finished = True
+    for k in range(3):
+        card_num = input("Pick a card (1-10)").strip()
+        while (card_num+1) < 0 or (card_num+1) > 10:
+            print("Invalid input! Enter a number between 1 and 10")
+            card_num = input("Pick a card (1-10)").strip()
+        if card_choices.rank(card_num) == "Ace":
+            player_points += 4
+        elif card_choices.rank(card_num) == "King":
+            player_points += 3
+        elif card_choices.rank(card_num) == "Queen":
+            player_points += 2
+        elif card_choices.rank(card_num) == "Jack":
+            player_points +=1
         else:
-            print("Restart!\n")
-
-    print(f"\n{bus_player.name} finished Riding the Bus!")
+            player_points += 0
+    print (f"\n{bus_player.name} finished Riding the Bus!")
+    return player_points
