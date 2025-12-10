f1498fccd69c9240b9d1de635d61f984f9f529e3
Carlos Miranda <canthonym2004@gmail.com>
2025-12-10 12:09:12 -0500

    fixed minor error and added docstrings to riding_the_bus

--- riding_the_bus.py
+++ riding_the_bus.py
@@ -2,10 +2,20 @@ import random
 def bus_player(players):
     """ Randomly determines the player who will ride the bus if at least two players have the lowest score.
     If else, then it will return the player with the lowest score.
+    
+    Args:
+        players (list[Player]): The player objects and their scores.
+        
+    Returns:
+        player(Player): The player object with the lowest score.
+    
+    Side effects:
+        Prints message if there are more than two losing players.
     """
     lowest_score = min(player.score for player in players)
     lowest_players = [player for player in players if player.score == lowest_score]
     if len(lowest_players) > 1:
+        print("There is a tie for the lowest score. A random player will be chosen to ride the bus")
         return random.choice(lowest_players)
     else:
         return lowest_players[0]
@@ -13,6 +23,16 @@ def bus_player(players):
 def ride_the_bus(deck, players):
     """ Player with lowest points is given 3 chances to choose between 10 cards.
     If the player gets a face card, they will gain points depending on the card given.
+    
+    Args:
+        deck(list[Card]): The card deck that will be used in the final round.
+        players (list[Player]): The player objects and their scores.
+        
+    Side effects:
+        Modifies deck by removing cards for round.
+        Requires input from the user to flip cards
+        Prints messages to inform the player
+        Modifies player's score.
     """
     
     bus_rider = bus_player(players)
@@ -27,7 +47,7 @@ def ride_the_bus(deck, players):
         14: 4,
         13: 3,
         12: 2,
-        1: 1
+        11: 1
     }
 
     points = 0
